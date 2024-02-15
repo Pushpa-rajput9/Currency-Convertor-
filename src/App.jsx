ba46a9551253cd0a3a704e491c8d2fbe89cf4c8c
pushpa rajput <pr4541285@gmail.com>
2024-02-15 10:23:13 +0530

    add deploy

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [convertedAmount, setConvertedAmount] = useState(0);
 
   const currencyInfo = useCurrencyInfo(from);
+  //const currencyinfoTo = useCurrencyInfo(to);
 
   const options = Object.keys(currencyInfo);
 
